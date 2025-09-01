ecd9ae5c217fcbca26cf06f732822414c8f45c76
Zakaria <zakariyae.mokhtari@gmail.com>
2025-09-01 17:16:49 +0200

    Delete test_transform.py

--- test_transform.py
+++
@@ -1,8 +0,0 @@
-import pprint
-from etl_pipeline.extract import fetch_weather
-from etl_pipeline.transform import transform_weather_data
-
-raw = fetch_weather("Stockholm")
-cleaned = transform_weather_data(raw)
-
-pprint.pprint(cleaned)
